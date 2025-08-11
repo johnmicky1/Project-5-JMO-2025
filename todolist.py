96e3dfc2c256b6722d3bfdbbef9d761583728f2a
John Michael <johnmicky1@gmail,com>
2025-08-11 16:48:23 +0300

    Initial Commit - Simple To-do-List

---
+++ todolist.py
@@ -0,0 +1,12 @@
+# Simple To-Do List Manager
+ 
+This is a Python project to manage tasks in a text file.
+ 
+## Features
+- Add new tasks
+- Remove tasks
+- Save tasks to a file
+ 
+## How to Run
+```bash
+python todo.py 
